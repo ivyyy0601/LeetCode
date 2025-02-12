5cc1bce0174592113abf6ea93b3bf91e1618ec05
IVY CHEN <ivyyy0601@gmail.com>
2025-02-12 10:56:25 +0800

    LC-binary-search [Runtime: N/A; Memory: N/A]

---
+++ binary-search/binary-search.py
@@ -0,0 +1,14 @@
+class Solution(object):#[left,right]
+    def search(self, nums, target):
+        left=0
+        right=len(nums)
+        while left<=right:
+            middle=(left+right)//2
+            if nums[middle]>target:
+                right=middle-1
+            elif nums[middle]<target:
+                left=middle+1
+            else:
+                return middle
+        return -1
+        
