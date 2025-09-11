0d5944c160b75844f5f1225dd6a91cd8b8cb6023
IVY CHEN <ivyyy0601@gmail.com>
2025-09-11 17:01:34 -0400

    LC-game-play-analysis-iv [Runtime: N/A; Memory: N/A]

--- database/game-play-analysis-iv.sql
+++ database/game-play-analysis-iv.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 select 
-round(count(*)/(SELECT COUNT(DISTINCT player_id) AS player_count FROM Activity),2) as fraction
+round(count(*)/(SELECT COUNT(DISTINCT player_id)  FROM Activity),2) as fraction
 from Activity
 where (player_id, event_date) not in 
 (select player_id, min(event_date)
@@ -9,3 +9,5 @@ where games_played != 0
 group by player_id)
 and games_played !=0
 group by player_id
+
+#COUNT() 里面不能直接塞一个 SELECT 子查询。
