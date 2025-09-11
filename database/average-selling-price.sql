d351cbf0a219f4783e4d7cff059c127082204187
IVY CHEN <ivyyy0601@gmail.com>
2025-09-11 09:52:17 -0400

    LC-average-selling-price [Runtime: 837 ms; Memory: 0B]

--- database/average-selling-price.sql
+++ database/average-selling-price.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select Prices.product_id, ifnull(ROUND(sum(price*units)/sum(units),2)) as average_price
+select Prices.product_id, ifnull(ROUND(sum(price*units)/sum(units),2),0) as average_price
 from Prices left join UnitsSold
 on Prices.product_id= UnitsSold.product_id and purchase_date between start_date and end_date
 group by Prices.product_id 
