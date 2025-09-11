cd5f44639142e0430f2c16ba9b9d30221457e336
IVY CHEN <ivyyy0601@gmail.com>
2025-09-11 17:23:45 -0400

    LC-game-play-analysis-iv [Runtime: 568 ms; Memory: 0B]

--- database/game-play-analysis-iv.sql
+++ database/game-play-analysis-iv.sql
@@ -7,9 +7,9 @@ from Activity
 where (player_id,DATE_SUB(event_date, INTERVAL 1 DAY))  in(
     select player_id, min(event_date)
     from Activity
-    where games_played != 0
+    #where games_played != 0 login就行
     group by player_id
 )
-and games_played != 0
-#DATE_SUB(event_date, INTERVAL 1 DAY)
+#and games_played != 0。  login就行
+#DATE_SUB(event_date, INTERVAL 1 DAY)  
 #COUNT() 里面不能直接塞一个 SELECT 子查询。
