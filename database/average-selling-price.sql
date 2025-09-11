aa936140a6f2a135ea498c39654ab23ca57b872c
IVY CHEN <ivyyy0601@gmail.com>
2025-09-11 09:51:36 -0400

    LC-average-selling-price [Runtime: N/A; Memory: N/A]

--- database/average-selling-price.sql
+++ database/average-selling-price.sql
@@ -1,5 +1,7 @@
 # Write your MySQL query statement below
-select Prices.product_id, ROUND(sum(price*units)/sum(units),2) as average_price
+select Prices.product_id, ifnull(ROUND(sum(price*units)/sum(units),2)) as average_price
 from Prices left join UnitsSold
 on Prices.product_id= UnitsSold.product_id and purchase_date between start_date and end_date
 group by Prices.product_id 
+#最后会变成这样子1          | null          |
+#IFNULL(..., 0) 遇到 NULL 的时候转成 0。
