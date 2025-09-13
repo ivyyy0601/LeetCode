f1296286e82c2ee57fbd740617ecbb8602513cf4
IVY CHEN <ivyyy0601@gmail.com>
2025-09-12 23:14:27 -0400

    LC-product-sales-analysis-iii [Runtime: 705 ms; Memory: 0B]

--- database/product-sales-analysis-iii.sql
+++ database/product-sales-analysis-iii.sql
@@ -6,3 +6,6 @@ where (product_id,year) in (
     from Sales
     group by product_id
     )
+
+-- ❌ WHERE year = MIN(year) 不行，因为在 WHERE 阶段还没算出 MIN(year)。
+-- ✅ (product_id, year) IN (子查询) 可以，因为子查询已经先算出了每个产品的最小年份。
