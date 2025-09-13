96ffac11cbef7f1dcd4e87c1925a9608e5fab02e
IVY CHEN <ivyyy0601@gmail.com>
2025-09-12 23:48:08 -0400

    LC-customers-who-bought-all-products [Runtime: 485 ms; Memory: 0B]

--- database/customers-who-bought-all-products.sql
+++ database/customers-who-bought-all-products.sql
@@ -2,7 +2,8 @@
 select customer_id
 from Customer 
 group by customer_id
-having count(product_key)=(
+having count(DISTINCT product_key)=(
     select count(*)
     from Product
 )
+#原因是题目说 Customer 表可能有重复行，同一顾客可能把同一个 product_key 记录多次
