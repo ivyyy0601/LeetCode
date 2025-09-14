fdaab5bf942d03e07b86c1fbab5b6961535c5d38
IVY CHEN <ivyyy0601@gmail.com>
2025-09-14 13:55:02 -0400

    LC-primary-department-for-each-employee [Runtime: 470 ms; Memory: 0B]

--- database/primary-department-for-each-employee.sql
+++ database/primary-department-for-each-employee.sql
@@ -1,18 +1,27 @@
 # Write your MySQL query statement below
 select employee_id, department_id
 from Employee 
-where (employee_id) in (
-    select employee_id
-    from Employee
-    group by employee_id
-    having count(department_id)>=2 
-) and primary_flag ='Y'
+where primary_flag ='Y' #一个yes就够；
 or (employee_id) in (
     select employee_id
     from Employee
     group by employee_id
-    having count(department_id) =1
-) and primary_flag ='N'
+    having count(department_id) =1 #一个count=1就够了！！一个就是n
+) 
+-- select employee_id, department_id
+-- from Employee 
+-- where (employee_id) in (
+--     select employee_id
+--     from Employee
+--     group by employee_id
+--     having count(department_id)>=2 
+-- ) and primary_flag ='Y' #一个yes就够；
+-- or (employee_id) in (
+--     select employee_id
+--     from Employee
+--     group by employee_id
+--     having count(department_id) =1 #一个count=1就够了！！
+-- ) and primary_flag ='N'
 
 
 
