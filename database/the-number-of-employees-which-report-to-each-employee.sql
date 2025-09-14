d82e835854826e447f45e43a026c298965ed79b1
IVY CHEN <ivyyy0601@gmail.com>
2025-09-14 13:34:05 -0400

    LC-the-number-of-employees-which-report-to-each-employee [Runtime: 618 ms; Memory: 0B]

--- database/the-number-of-employees-which-report-to-each-employee.sql
+++ database/the-number-of-employees-which-report-to-each-employee.sql
@@ -1,10 +1,6 @@
 # Write your MySQL query statement below
-select  m.employee_id, m.name,  count(e.employee_id) as reports_count ,round(avg(e.age)) as average_age
-from Employees m  join Employees e
-on m.employee_id=e.reports_to
+select m.employee_id ,  m.name , count(e.employee_id) as reports_count, round (avg(e.age)) as average_age
+from Employees m join Employees e
+on m.employee_id = e.reports_to
 group by m.employee_id
-ORDER BY m.employee_id
-#
- 
-
-
+order by  m.employee_id
