4e06c86155768974f94b1f0628d9bb8105515596
IVY CHEN <ivyyy0601@gmail.com>
2025-09-14 14:40:48 -0400

    LC-product-price-at-a-given-date [Runtime: N/A; Memory: N/A]

--- database/product-price-at-a-given-date.sql
+++ database/product-price-at-a-given-date.sql
@@ -1,8 +1,8 @@
 # Write your MySQL query statement below
 select 
 product_id,
-case when change_date <= '2019-08-16' then new_price
-else 10 end as price
+case when change_date > '2019-08-16' then 10
+else new_price end as price
 from Products
 where (product_id,change_date) in
 (
@@ -15,7 +15,6 @@ or (product_id,change_date) in
 (
     select product_id, change_date
     from Products
-    where change_date > '2019-08-16'
     group by product_id
-    having count(product_id)=1 
+    having count(product_id)=1
 )
