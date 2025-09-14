e9d0737cca2907f8472e9f3c87d0f263f2264287
IVY CHEN <ivyyy0601@gmail.com>
2025-09-14 15:11:17 -0400

    LC-product-price-at-a-given-date [Runtime: 405 ms; Memory: 0B]

--- database/product-price-at-a-given-date.sql
+++ database/product-price-at-a-given-date.sql
@@ -4,14 +4,14 @@ distinct product_id,
 case when change_date > '2019-08-16' then 10
 else new_price end as price
 from Products
-where (product_id,change_date) in
+where (product_id,change_date) in # 截止日前最后一次
 (
     select product_id, max(change_date)
     from Products
     where change_date <= '2019-08-16'
     group by product_id
 ) 
-or (product_id,change_date) in
+or (product_id,change_date) in #在截至日期后 但是从来都没有出现过在截止日前 所以就是10 而且这个product我们只记录一次
 (
     select  distinct product_id ,change_date
     from Products
@@ -22,3 +22,8 @@ or (product_id,change_date) in
         where change_date <= '2019-08-16'
     )
 )
+
+
+--  (product_id, new_price, change_date) IN (
+--         SELECT DISTINCT product_id, 10, change_date
+-- 这意味着只有当表里真的存在 new_price=10 的那行时才会匹配；如果表里没出现过 10，就不会返回这类产品（违背“默认价=10”的本意）
