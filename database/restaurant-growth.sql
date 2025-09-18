e5b52c0926f103d226f02c627180f4d3e7afa42a
IVY CHEN <ivyyy0601@gmail.com>
2025-09-18 06:55:44 -0400

    LC-restaurant-growth [Runtime: 344 ms; Memory: 0B]

--- database/restaurant-growth.sql
+++ database/restaurant-growth.sql
@@ -1,21 +1,20 @@
 # Write your MySQL query statement below
-#难！！！！！！！！！1
+#难！！！！！！！！！1 #
 select visited_on,
 (
     select sum(amount)
     from Customer
-    where visited_on BETWEEN DATE_SUB(c.visited_on, INTERVAL 6 DAY) AND c.visited_on
-)as amount,ROUND(
-    (
-        select sum(amount)/7
-        from Customer
-        where visited_on BETWEEN DATE_SUB(c.visited_on, INTERVAL 6 DAY) AND c.visited_on
-    )
-,2)as average_amount
+    where visited_on between date_sub(c.visited_on, interval 6 day) and c.visited_on
+) as amount,
+(
+    select round(sum(amount)/7,2) 
+    from Customer
+    where visited_on between date_sub(c.visited_on, interval 6 day) and c.visited_on
+) as average_amount #题目要“最近 7 天的日均金额”，应该是 7 天总额 / 7，而不是对所有行 AVG(amount)。avr是对于全部行来讲的
 from Customer c
-where c.visited_on >= (
-    select DATE_ADD(MIN(visited_on), INTERVAL 6 DAY)# ③ 只从第 7 天开始输出（保证窗口凑满 7 天）
-    from customer
+where c.visited_on >=
+(
+    select date_add(min(visited_on), interval 6 day)
+    from Customer
 )
-group by visited_on 
-# 由于 customer 可能一天多行，这里按天去重成一行()
+group by visited_on
