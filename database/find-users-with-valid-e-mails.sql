9daeabce722f970a2a8cb2af38af6ee35f352d45
IVY CHEN <ivyyy0601@gmail.com>
2025-09-20 22:57:38 -0400

    LC-find-users-with-valid-e-mails [Runtime: N/A; Memory: N/A]

--- database/find-users-with-valid-e-mails.sql
+++ database/find-users-with-valid-e-mails.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
 select user_id, name, mail
 from Users
-where mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode[.]com$'
+where mail REGEXP  '^[a-zA-Z][a-zA-Z0-9_.-]@leetcode\\.com$'
+#：REGEXP BINARY（强制大小写敏感）
