047aa6d58875a7f3e2b38df668b729cc8143fea6
IVY CHEN <ivyyy0601@gmail.com>
2025-10-09 11:43:59 -0400

    LC-next-greater-element-i [Runtime: 295 ms; Memory: 18.2 MB]

--- stack/next-greater-element-i.py
+++ stack/next-greater-element-i.py
@@ -2,20 +2,20 @@ class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         stack=[]
         result=[] 
-        temp=[]
-        found=0
-        res=-1
         for n2 in nums2:
             stack.append(n2)
         for n1 in nums1:
+            temp=[]
+            found=0
+            res=-1
             while len(stack)!=0 and found==0:
                 top=stack.pop()
-                if top>n1:
-                    res=max(top,res)
-                else:
-                    found=1
                 temp.append(top)
+                if top>n1:
+                    res=top
+                elif top==n1:
+                    found=1
             result.append(res)
-        while len(temp)!=0:
-            stack.append(temp.pop())
+            while len(temp)!=0:
+                stack.append(temp.pop())
         return result
