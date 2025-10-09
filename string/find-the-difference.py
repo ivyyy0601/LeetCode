d025dbeacb1a1eeb047f127117d55d8f2e8f3b56
IVY CHEN <ivyyy0601@gmail.com>
2025-10-09 12:58:43 -0400

    LC-find-the-difference [Runtime: 0 ms; Memory: 18 MB]

--- string/find-the-difference.py
+++ string/find-the-difference.py
@@ -4,15 +4,9 @@ class Solution:
         b={}
         string=""
         for n1 in s:
-            if n1 in a:
-                a[n1]+=1
-            else:
-                a[n1]=1
+            a[n1]=a.get(n1,0)+1
         for n2 in t:
-            if n2 in b:
-                b[n2]+=1
-            else:
-                b[n2]=1  
+            b[n2]=b.get(n2,0)+1
         for k,v in a.items():
             if k in b:
                 b[k]=b[k]-v
