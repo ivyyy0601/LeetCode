cf0727502e4362138af59b3c753c5399155a0877
IVY CHEN <ivyyy0601@gmail.com>
2025-10-09 14:26:30 -0400

    LC-contains-duplicate [Runtime: 13 ms; Memory: 31.6 MB]

--- sorting/contains-duplicate.py
+++ sorting/contains-duplicate.py
@@ -1,13 +1,15 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        dict={}
-        for n in nums:
-            if n in dict:
-                dict[n]+=1
-            else:
-                dict[n]=1
-        for value in dict.values():
-            if value >1:
-                return True
-        return False
-        
+        # dict={}
+        # for n in nums:
+        #     if n in dict:
+        #         dict[n]+=1
+        #     else:
+        #         dict[n]=1
+        # for value in dict.values():
+        #     if value >1:
+        #         return True
+        # return False
+        #用set快很多
+        s=set(nums)
+        return len(s) != len(nums)
