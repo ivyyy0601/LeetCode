eb19bdc18577c278458a5e5f1b6e6356cb07340a
IVY CHEN <ivyyy0601@gmail.com>
2025-10-08 22:33:03 -0400

    LC-move-zeroes [Runtime: 3 ms; Memory: 18.8 MB]

--- two-pointers/move-zeroes.py
+++ two-pointers/move-zeroes.py
@@ -1,6 +1,15 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        for i in range(0,len(nums)):
-            if nums[i]==0:
-                nums.pop(i)
-                nums.append(0)
+        # for i in range(len(nums)): 
+        #一旦 pop(i) 删除当前元素，后面的元素会整体左移，接着 i 还会自增，结果就跳过了紧挨着的那个元素。所以不行
+        #     if nums[i]==0: 
+        #         nums.pop(i)
+        #         nums.append(0)
+        #（两指针，原地，稳定顺序，O(n)）
+        index=0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[index]=nums[i]
+                index+=1
+        for i in range(index,len(nums)):
+                nums[i]=0
