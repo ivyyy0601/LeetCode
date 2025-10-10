4b23bf4e012ce043665af9b0d983f7d7298b3b35
IVY CHEN <ivyyy0601@gmail.com>
2025-10-10 16:56:21 -0400

    LC-maximum-number-of-vowels-in-a-substring-of-given-length [Runtime: N/A; Memory: N/A]

--- sliding-window/maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ sliding-window/maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -3,9 +3,9 @@ class Solution:
         #a, e, i, o, u
         left=0
         result=0
-        for right in range(k,len(s)):
+        for right in range(k,len(s)+1): #right 最后一次取的是 len(s)-1，永远到不了 len(s)。
             count=0
-            for element in s[left:right]:
+            for element in s[left:right]: #这里又不包括 len(s)-1 就是他的真正要取的值
                 if element=="a" or element=="e" or element=="i" or element=="o" or element=="u":
                     count+=1
             result=max(result,count)
