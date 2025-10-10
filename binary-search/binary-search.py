d634d4e910d4d133176b738516b4a7298e3b2217
IVY CHEN <ivyyy0601@gmail.com>
2025-10-10 13:26:25 -0400

    LC-binary-search [Runtime: N/A; Memory: N/A]

--- binary-search/binary-search.py
+++ binary-search/binary-search.py
@@ -1,14 +1,16 @@
-class Solution(object):#[left,right）
+class Solution(object):
     def search(self, nums, target):
-        left=0
-        right=len(nums)
-        while left<right:
-            middle=(left+right)//2
-            if (nums[middle]>target):
-                right=middle
-            elif (nums[middle]<target):
-                left=middle+1
+        if nums is None or len(nums)==0:
+            return -1
+        l=0
+        r=len(nums)-1
+        mid=r+l/2
+        while r>l:
+            mid=r+l/2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]>target:
+                r=mid-1
             else:
-                return middle
+                l= mid+1
         return -1
-        
