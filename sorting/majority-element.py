2b99977c7147b98197d0a56b3a284f493af94a1f
IVY CHEN <ivyyy0601@gmail.com>
2025-10-11 10:13:31 -0400

    LC-majority-element [Runtime: 0 ms; Memory: 19.3 MB]

--- sorting/majority-element.py
+++ sorting/majority-element.py
@@ -10,27 +10,27 @@ class Solution:
         #         return k
         # return 0
         #法2️⃣： 如果一个数出现次数超过一半（> n/2）
-        # nums.sort()
-        # return nums[len(nums)//2]
+        nums.sort()
+        return nums[len(nums)//2]
         #法3️⃣ divide & conquer
 
-        def getmaj(left,right) -> int:
-            if left==right:
-                return nums[left]
-            mid=(left+right)//2
-            left_mar = getmaj(left, mid)
-            right_mar= getmaj(mid+1,right)
-            if left_mar==right_mar:
-                return left_mar
-            left_count=0
-            right_count=0
-            for i in nums:
-                if i==left_mar:
-                    left_count+=1
-                elif i == right_mar:
-                    right_count+=1
-            if left_count>=right_count:
-                return left_mar
-            else:
-                return right_mar
-        return getmaj(0,len(nums)-1)
+        # def getmaj(left,right) -> int:
+        #     if left==right:
+        #         return nums[left]
+        #     mid=(left+right)//2
+        #     left_mar = getmaj(left, mid)
+        #     right_mar= getmaj(mid+1,right)
+        #     if left_mar==right_mar:
+        #         return left_mar
+        #     left_count=0
+        #     right_count=0
+        #     for i in nums:
+        #         if i==left_mar:
+        #             left_count+=1
+        #         elif i == right_mar:
+        #             right_count+=1
+        #     if left_count>=right_count:
+        #         return left_mar
+        #     else:
+        #         return right_mar
+        # return getmaj(0,len(nums)-1)
