383ef5ad4d567fd1b49b56910e987e6f370ae5da
IVY CHEN <ivyyy0601@gmail.com>
2025-10-11 00:08:37 -0400

    LC-reverse-linked-list [Runtime: 0 ms; Memory: 18.9 MB]

--- recursion/reverse-linked-list.py
+++ recursion/reverse-linked-list.py
@@ -5,13 +5,19 @@ class ListNode:
         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        dummy= ListNode()
-        dummy.next=head
-        while head is not None and head.next is not None:
-            dnext=dummy.next
-            hnext=head.next
-            dummy.next= hnext
-            head.next = hnext.next
-            hnext.next=dnext
-        return dummy.next
-        
+        # dummy= ListNode()
+        # dummy.next=head
+        # while head is not None and head.next is not None:
+        #     dnext=dummy.next
+        #     hnext=head.next
+        #     dummy.next= hnext
+        #     head.next = hnext.next
+        #     hnext.next=dnext      #画图理解
+        # return dummy.next
+        #递归的方法
+        if head is None or head.next is None:
+            return head
+        result=self.reverseList(head.next)
+        head.next.next=head
+        head.next= None
+        return result
