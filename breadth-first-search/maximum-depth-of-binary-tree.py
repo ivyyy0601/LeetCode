529b9a1155a432e75d880e068c76f0413038d7ec
IVY CHEN <ivyyy0601@gmail.com>
2025-10-26 11:02:20 -0400

    LC-maximum-depth-of-binary-tree [Runtime: N/A; Memory: N/A]

--- breadth-first-search/maximum-depth-of-binary-tree.py
+++ breadth-first-search/maximum-depth-of-binary-tree.py
@@ -6,7 +6,7 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        # #递归 前中后
+        # #1. 递归 前中后 容易理解 dfs
         # def depth(root):
         #     if not root:
         #         return 0
@@ -15,8 +15,28 @@ class Solution:
         #     return max(left,right)+1
 
         # return depth(root)
+        #简易的dfs
+        # if not root:
+        #     return 0
+        # return max(self.maxDepth(root.left),self.maxDepth(root.right))+1
+        #用bfs也可以 就是要queue
         if not root:
-            return 0
-        return max(self.maxDepth(root.left),self.maxDepth(root.right))+1
+            return 1
+        #初始化
+        max_depth=0 #一个节点就是1了
+        queue=deque()
+        queue.append(root)
+
+        while queue:
+            nums=len(queue)
+            for i in range(nums):
+                element= queue.popleft()
+                if element.left:
+                    queue.append(element.left)
+                if element.right:
+                    queue.append(element.right)
+                    #nums=len(queue) #该层有的节点
+            max_depth+=1 #结束该层 就depth+1
+        return max_depth
 
         
