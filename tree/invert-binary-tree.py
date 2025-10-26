23e1ea5a68dfb9fad505017d6aa7607bc8af59d7
IVY CHEN <ivyyy0601@gmail.com>
2025-10-26 11:59:22 -0400

    LC-invert-binary-tree [Runtime: 0 ms; Memory: 17.8 MB]

--- tree/invert-binary-tree.py
+++ tree/invert-binary-tree.py
@@ -8,12 +8,27 @@ class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         #dfs或者bfs都可以
         #dfs
-        def reverse(root):
-            if not root:
-                return
-            root.left, root.right = root.right, root.left
-            reverse(root.left)
-            reverse(root.right)
-        reverse(root)
+        # def reverse(root):
+        #     if not root:
+        #         return
+        #     root.left, root.right = root.right, root.left
+        #     reverse(root.left)
+        #     reverse(root.right)
+        # reverse(root)
+        # return root
+        #bfs
+        if not root:
+            return 
+        queue=deque([root])
+        while queue:
+            # ele_len=len(queue)
+            # for _ in range(ele_len):#因为我们只需要管每个元素 不需要说管每一层怎么样
+            element=queue.popleft()
+            element.left, element.right = element.right, element.left #取出来的操作
+            if element.left:
+                queue.append(element.left)
+            if element.right:
+                queue.append(element.right)
+            
         return root
         
