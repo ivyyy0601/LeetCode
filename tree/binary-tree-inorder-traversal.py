e2a6dc292b379b561c0ebeda41e04cac4f9d1381
IVY CHEN <ivyyy0601@gmail.com>
2025-10-26 10:33:34 -0400

    LC-binary-tree-inorder-traversal [Runtime: 0 ms; Memory: 17.8 MB]

--- tree/binary-tree-inorder-traversal.py
+++ tree/binary-tree-inorder-traversal.py
@@ -6,14 +6,28 @@ class TreeNode:
         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        def dfs(node):
-            if node is None:
-                return
-            dfs(node.left)
-            res.append(node.val)
-            dfs(node.right)
-        res=[]
-        dfs(root)
+        # #递归的形式
+        # def dfs(node):
+        #     if node is None:
+        #         return
+        #     dfs(node.left)
+        #     res.append(node.val)
+        #     dfs(node.right)
+        # res=[]
+        # dfs(root)
+        # return res
+        #非递归： 一路向left 放到stack 里面，然后到底之后pop出stack的东西 存到result里面 然后往右边看
+        res=[] #存的是数值
+        stack=[]
+        while root or stack:
+            while root:
+                stack.append(root)
+                root=root.left
+
+            element=stack.pop()
+            res.append(element.val)
+            root=element.right
+
         return res
         
 
