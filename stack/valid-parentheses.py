63646341d1aa7c15840c0ad7480f0e926301880d
IVY CHEN <ivyyy0601@gmail.com>
2025-10-27 08:33:30 -0400

    LC-valid-parentheses [Runtime: 1 ms; Memory: 17.8 MB]

--- stack/valid-parentheses.py
+++ stack/valid-parentheses.py
@@ -2,22 +2,19 @@ class Solution:
     #你在循环中每次都在修改 flag，但是如果最后一次括号刚好匹配上，就会让 flag=1，即使前面有不匹配的情况，也被覆盖掉. 用flase就行
     def isValid(self, s: str) -> bool:
         stack=[]
-        for char in s:
-            if char=='(' or char=='{' or char=='[':
-                stack.append(char)
+        for ch in s:
+            if ch == '(' or ch=='{' or ch=='[':
+                stack.append(ch)
             else:
-                if len(stack)==0:
+                if not stack:
                     return False
-                else:
-                    if char==')' and stack.pop()!='(':
-                        return False
-                    elif char==']' and stack.pop()!='[':
-                        return False
-                    elif char=='}' and stack.pop()!='{':
-                        return False
-        if len(stack)==0:
-            return True
-        else:
+                stack_ele=stack.pop()
+                if stack_ele=='(' and ch !=')':
+                    return False
+                elif stack_ele=='{' and ch !='}':
+                    return False
+                elif stack_ele=='[' and ch !=']':
+                    return False
+        if stack:
             return False
-
-        
+        return True
