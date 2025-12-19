f2e26c7dc89b4257eecce0b998fb31387fa4c23d
IVY CHEN <ivyyy0601@gmail.com>
2025-12-19 10:59:06 -0500

    LC-article-views-i [Runtime: N/A; Memory: N/A]

--- database/article-views-i.py
+++ database/article-views-i.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 def article_views(views: pd.DataFrame) -> pd.DataFrame:
     read_self = views[views['author_id']==views['viewer_id']]
-    group = read_self.groupby("author_id")["article_id"].count().reset_index()
-    return group[["author_id"]].rename(columns={"author_id":"id"}).sort_values("id"))
+    #group = read_self.groupby("author_id")["article_id"].count().reset_index()[["author_id"]].rename(columns = {"author_id":"id"}).sort_values("id")
+    #当你要用count的sort的时候再使用！！
+    group = read_self.drop_duplicates().sort_values('author_id').rename(columns={"author_id": "id"})
+    return group[["id"]]
     
