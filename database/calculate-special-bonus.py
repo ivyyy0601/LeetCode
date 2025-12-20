413a00461cc25052e4b192061e79373e84278c1f
IVY CHEN <ivyyy0601@gmail.com>
2025-12-20 03:27:31 -0500

    LC-calculate-special-bonus [Runtime: 315 ms; Memory: 68 MB]

--- database/calculate-special-bonus.py
+++ database/calculate-special-bonus.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
 def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
-    res = employees[(^(employees["name"].str.startswith("M")) &  (employees["employee_id"] % 2 = 1)]
-    return res
+    no = employees[(employees["name"].str.startswith("M")) | (employees["employee_id"] % 2 == 0)]
+    yes = employees[(~employees["name"].str.startswith("M")) & (employees["employee_id"] % 2 == 1)]
+    no["salary"] = 0
+    res = pd.concat([yes,no], ignore_index=True)[["employee_id","salary"]].sort_values("employee_id")
+    return res.rename(columns={"salary":"bonus"})
     
